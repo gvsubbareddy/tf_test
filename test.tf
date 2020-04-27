92609146b375d60ccaa182ce25236237dea96c90
Subba Reddy <subbareddy.guvvala@gmail.com>
2020-04-27 22:22:54 +0530

    Update test.tf

--- test.tf
+++ test.tf
@@ -1,9 +1,3 @@
-provider "aws" {
-  access_key = "AKIAUQBEO7OG2Q73SPGC"
-  secret_key = "SIU2/cpe9K84BuWtQ98ZiZ5cOGJKrdIqCqiPmc0e"
-  region     = "us-east-2"
-}
-
 resource "aws_instance" "example" {
   ami           = "ami-0520e698dd500b1d1"
   instance_type = "t2.micro"
