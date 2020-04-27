08279a4c1862ea0de9a5c1db0846a756b06fc1d6
Subba Reddy <subbareddy.guvvala@gmail.com>
2020-04-27 22:58:53 +0530

    Update test.tf

--- test.tf
+++ test.tf
@@ -1,3 +1,9 @@
+provider "aws" {
+  access_key = "${env.AWS_ACCESS_KEY_ID}"
+  secret_key = "${env.AWS_SECRET_ACCESS_KEY}"
+  region     = "us-east-2"
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-0520e698dd500b1d1"
   instance_type = "t2.micro"
